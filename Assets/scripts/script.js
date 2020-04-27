1959f80be9ab36bf0c4f2682ec0bd6e005bc8e47
Andrew Hansbrough <hansbroughku@gmail.com>
2020-04-26 21:30:21 -0500

    Practiced console.logging through the api cobweb. Fixed style.css. JS file contains current and 5 day forecast ajax calls with console logs.

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -5,8 +5,84 @@ var APIKey = "857c3a0a6b4e70819e79df943dea660d";
 // Here we are building the URL we need to query the database
 var queryURL =
   "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=Bujumbura,Burundi&units=imperial&appid=" +
+  "q=Kansas+City&units=imperial&appid=" +
   APIKey;
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+})
+  // Store all retrieved data in response
+  .then(function (response) {
+    // Log the queryURL
+    console.log(queryURL);
+    // Log the resulting object
+    console.log(response);
+    //Transfer Content to HTML
+    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    $(".temp").text("Temperature (F) " + response.main.temp);
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    //Log Data in Console
+    console.log(response.name);
+    console.log(response.dt);
+    console.log(response.main.temp);
+    console.log(response.main.humidity);
+    console.log(response.wind.speed);
+    console.log(response.weather[0].main);
+    console.log(response.coord.lon);
+    console.log(response.coord.lat);
+  });
+
+var APIKey = "857c3a0a6b4e70819e79df943dea660d";
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/forecast?" +
+  "q=Kansas+City&appid=" +
+  APIKey;
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function (response) {
+  // Log the queryURL
+  console.log(queryURL);
+  // Log the resulting object
+  console.log(response.list);
+  console.log(response.list[5].dt_txt);
+  console.log(response.list[5].weather[0].id);
+  console.log(response.list[5].main.temp);
+  console.log(response.list[5].main.humidity);
+  console.log(response.list[13].dt_txt);
+  console.log(response.list[13].weather[0].id);
+  console.log(response.list[13].main.temp);
+  console.log(response.list[13].main.humidity);
+  console.log(response.list[21].dt_txt);
+  console.log(response.list[21].weather[0].id);
+  console.log(response.list[21].main.temp);
+  console.log(response.list[21].main.humidity);
+  console.log(response.list[29].dt_txt);
+  console.log(response.list[29].weather[0].id);
+  console.log(response.list[29].main.temp);
+  console.log(response.list[29].main.humidity);
+  console.log(response.list[37].dt_txt);
+  console.log(response.list[37].weather[0].id);
+  console.log(response.list[37].main.temp);
+  console.log(response.list[37].main.humidity);
+});
+
+//UV Index Bad Request. Make sure You Do this last, after everything else work.
+//var APIKey = "857c3a0a6b4e70819e79df943dea660d";
+//var uvURL =
+//  "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+//  APIKey +
+//  "&lat={lat}&lon={lon}";
+//
+//$.ajax({
+//  url: uvURL,
+//  method: "GET",
+//}).then(function (response) {
+//  console.log(response);
+//});
 
 //Capture form input: Add new city with button, get current and future forecasts. Establish form input first.
 
@@ -23,33 +99,6 @@ var queryURL =
 //New cities are added to list when searched and will be displayed in search history.
 
 //Run AJAX call to the OpenWeatherMap API
-$.ajax({
-  url: queryURL,
-  method: "GET",
-})
-  // Store all retrieved data in response
-  .then(function (response) {
-    // Log the queryURL
-    console.log(queryURL);
-
-    // Log the resulting object
-    console.log(response);
-
-    //Transfer Content to HTML
-    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-    $(".temp").text("Temperature (F) " + response.main.temp);
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-    $(".humidity").text("Humidity: " + response.main.humidity);
-
-    //Log Data in Console
-    console.log(response.name);
-    console.log(response.dt);
-    console.log(response.main.temp);
-    console.log(response.main.humidity);
-    console.log(response.wind.speed);
-    console.log(response.weather[0].main);
-  });
-
 //Search Button On click Function
 
 $("#searchBtn").on("click", function (event) {
